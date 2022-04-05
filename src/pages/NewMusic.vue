ddb2addad74f517986c83b5d852bde9702fe737e
nazocthun <nazocthun@gmail.com>
2022-04-05 22:29:10 +0800

    text overflow & :title

--- src/pages/NewMusic.vue
+++ src/pages/NewMusic.vue
@@ -24,38 +24,38 @@
 
         <el-table-column label="音乐标题" width="">
           <template #default="scope">
-            <div inline-block text-gray-600 cursor-default>
+            <span text-gray-600 cursor-default overflow-hidden text-ellipsis whitespace-nowrap :title="scope.row.name">
               {{ scope.row.name }}
-            </div>
+            </span>
           </template>
         </el-table-column>
 
         <el-table-column label="歌手" width="">
           <template #default="scope">
-            <div v-for="(artist, i) in scope.row.artists" :key="i" inline-block>
-              <span cursor-pointer text-sky-600 @click="toArtist(artist.id)">{{ artist.name }}</span>
-              <span v-show="scope.row.artists.length != 1 && i!=scope.row.artists.length-1" text-gray-600 cursor-default>
-                &amp;&nbsp;
+            <span :title="artistsTitleString(scope.row.artists)">
+              <span v-for="(artist, i) in scope.row.artists" :key="i" overflow-hidden text-ellipsis whitespace-nowrap>
+                <span cursor-pointer text-sky-600 @click="toArtist(artist.id)">{{ artist.name }}</span>
+                <span v-show="scope.row.artists.length != 1 && i!=scope.row.artists.length-1" text-gray-600 cursor-default>&nbsp;&amp;&nbsp;</span>
               </span>
-            </div>
+            </span>
           </template>
         </el-table-column>
 
         <el-table-column prop="album" label="专辑" width="">
-          <template flex #default="scope">
-            <div flex justify-start items-center>
-              <div
-                flex-initial cursor-pointer text-sky-600 mr-auto
+          <template #default="scope">
+            <span flex justify-start items-center :title="scope.row.album.name">
+              <span
+                flex-initial cursor-pointer text-sky-600 mr-auto pr-4 overflow-hidden text-ellipsis whitespace-nowrap
                 @click="toAlbum(scope.row.album.id)"
               >
                 {{ scope.row.album.name }}
-              </div>
-              <div
+              </span>
+              <span
                 flex float-right mr-4 cursor-pointer w-6 h-6 hover:text-orange-700
                 i-ic-sharp-playlist-add
                 @click="addToQueue(scope.row, 'plus')"
               />
-            </div>
+            </span>
           </template>
         </el-table-column>
         <el-table-column prop="time" label="时长" width="100" />
@@ -67,6 +67,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { artistsTitleString } from '@/utils/common'
 import usePlay from '@/hooks/usePlay'
 import { getNewMusic } from '@/api/getNewMusic'
 import useMusicQueue from '@/hooks/useMusicQueue'
@@ -99,11 +100,9 @@ const { addToQueue } = useMusicQueue()
 // 路由跳转
 const router = useRouter()
 function toArtist(id: any) {
-  console.log(id)
   router.push(`/artist?artistId=${id}`)
 }
 function toAlbum(id: any) {
-  console.log(id)
   router.push(`/album?id=${id}`)
 }
 </script>
