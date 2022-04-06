5b474a363a86ca78cb6e149f97cf82e8e33b35e8
nazocthun <nazocthun@gmail.com>
2022-04-07 05:43:17 +0800

    feat: MusicTable in album page

--- src/pages/AlbumPage.vue
+++ src/pages/AlbumPage.vue
@@ -41,40 +41,7 @@
     <el-tabs v-model="activeName" @tab-click="handleClick">
       <el-tab-pane label="歌曲列表" name="music">
         <div w-full>
-          <el-table :data="tableData" stripe @row-dblclick="rowDoubleClick">
-            <el-table-column type="index" width="50" />
-            <el-table-column prop="name" label="音乐标题" width="" />
-            <el-table-column prop="artists" label="歌手" width="">
-              <template #default="scope">
-                <span :title="artistsTitleString(scope.row.artists)">
-                  <span v-for="(artist, i) in scope.row.artists" :key="i" overflow-hidden text-ellipsis whitespace-nowrap>
-                    <span cursor-pointer text-sky-600 @click="toArtist(artist.id)">{{ artist.name }}</span>
-                    <span v-show="scope.row.artists.length != 1 && i!=scope.row.artists.length-1" text-gray-600 cursor-default>&nbsp;&amp;&nbsp;</span>
-                  </span>
-                </span>
-              </template>
-            </el-table-column>
-
-            <el-table-column prop="album" label="专辑">
-              <template #default="scope">
-                <span flex justify-start items-center :title="scope.row.album.name">
-                  <span
-                    flex-initial cursor-pointer text-sky-600 mr-auto pr-4 overflow-hidden text-ellipsis whitespace-nowrap
-                    @click="toAlbum(scope.row.album.id)"
-                  >
-                    {{ scope.row.album.name }}
-                  </span>
-                  <span
-                    flex float-right mr-4 cursor-pointer w-6 h-6 hover:text-orange-700
-                    i-ic-sharp-playlist-add
-                    @click="addToQueue(scope.row, 'plus')"
-                  />
-                </span>
-              </template>
-            </el-table-column>
-
-            <el-table-column prop="time" label="时长" width="100" />
-          </el-table>
+          <MusicTable :data="tableData" :loading="false" :pic="false" :album="false" />
         </div>
       </el-tab-pane>
       <el-tab-pane label="专辑简介" name="descrption">
@@ -98,7 +65,6 @@ import { useMusicQueueStore } from '@/store/MusicQueueStore'
 import { usePlayStore } from '@/store/PlayStore'
 import usePlay from '@/hooks/usePlay'
 import useMusicQueue from '@/hooks/useMusicQueue'
-import { artistsTitleString } from '@/utils/common'
 
 const MUSIC_INFO_STORE = useMusicInfoStore()
 const MUSIC_QUEUE_STORE = useMusicQueueStore()
@@ -165,15 +131,6 @@ function playAll() {
 
 // 路由跳转
 const route = useRoute()
-const router = useRouter()
-function toArtist(id: number) {
-  console.log(id)
-  router.push(`/artist?artistId=${id}`)
-}
-function toAlbum(id: number) {
-  console.log(id)
-  router.push(`/album?id=${id}`)
-}
 
 // 监视路由变化
 watch(() => route, (newValue) => {
