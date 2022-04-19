b2dcf5481664cf86db068840e7722d78b04e8d80
nazocthun <nazocthun@gmail.com>
2022-04-20 01:11:39 +0800

    feat: file-based router

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import { createPinia } from 'pinia'
+import routes from 'virtual:generated-pages'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 import './styles/main.css'
 import 'uno.css'
 import '@/styles/custom.scss'
 
 const app = createApp(App)
 const pinia = createPinia()
-
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
 app.use(router).use(pinia)
 
 app.mount('#app')
