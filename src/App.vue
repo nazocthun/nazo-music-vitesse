21457f6fe30d369b38d3b322f43e8e4089253119
nazocthun <nazocthun@gmail.com>
2022-04-23 10:34:55 +0800

    fix: remove MainPage style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <el-container direction="vertical">
     <NavBar class="bg-slate-300/50" relative items-center />
-    <MainPage class="fixed top-16 bottom-16 w-full" />
+    <MainPage class="mainpage fixed top-16 bottom-16 w-full" />
     <BottomZone class="bg-slate-300/50" fixed bottom-0 h-16 w-full />
   </el-container>
   <!-- <Footer /> -->
@@ -21,5 +21,8 @@
 html, body{
   margin:0;
   height:100%;
+}
+.mainpage {
+  height: calc(100vh - 128px);
 }
 </style>
