317c980b15545d594a620d78cbefb8454d4d0e75
nazocthun <nazocthun@gmail.com>
2022-04-23 10:41:00 +0800

    feat: router push

--- src/pages/Index/SelectedLists.vue
+++ src/pages/Index/SelectedLists.vue
@@ -5,41 +5,10 @@
       <el-tab-pane label="欧美" name="欧美" />
       <el-tab-pane label="华语" name="华语" />
     </el-tabs>
-    <!-- <div>
-      <ul
-        v-infinite-scroll="loadMore"
-        infinite-scroll-delay="700"
-        grid grid-cols-5 gap-5 items-center list-none pl-0 m-3
-      >
-        <li v-for="(songList, index1) in songListsData" :key="index1" text-sm w-full>
-          <div class="group" w-full aspect-square relative cursor-pointer hover="scale-105 transition-all" @click="toSongList(songList.id)">
-            <CoverLazy :src="songList.picUrl" />
-            <div
-              absolute justify-center items-center
-              w-12 h-12 top="50%" left="50%" translate-x="-50%" translate-y="-50%"
-              m-0 rounded-full opacity-0 group-hover="bg-white opacity-100 transition-all"
-              @click.stop="playSongList(songList.id)"
-            >
-              <div
-                w-full h-full bg-orange-700
-                i-ic-sharp-play-circle-outline
-              />
-            </div>
-          </div>
-          <div :title="songList.name" my="1.5" overflow-hidden text-ellipsis whitespace-nowrap text-sm>
-            {{ songList.name }}
-          </div>
-          <div text-xs>
-            {{ songList.createTime }}
-          </div>
-        </li>
-      </ul>
-    </div>
-    <div v-if="showMore && more" h-16>加载中</div>
-    <div v-if="!more" h-16>没有更多了</div> -->
     <CoverView
       :data="songListsData"
       :more="more"
+      :show-more="true"
       @load-more="loadMore"
       @play="playSongList"
       @to="toSongList"
@@ -49,6 +18,7 @@
 <script setup lang="ts">
 import { getSelectedSongList } from '@/api/getSongListInfo'
 
+const router = useRouter()
 const loading = ref(true)
 const activeName = ref('全部')
 const more = ref(true)
@@ -59,6 +29,7 @@ function playSongList(id: number) {
 }
 
 function toSongList(id: number) {
+  router.push(`/list?id=${id}`)
   console.log(`toSongList${id}`)
 }
 const params = reactive({
