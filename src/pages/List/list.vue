ea93ef25cd2cb3c62d7dccd2bf0ebacd6b8c400a
nazocthun <nazocthun@gmail.com>
2022-04-23 10:40:24 +0800

    feat: list with infinite loading

--- src/pages/List/list.vue
+++ src/pages/List/list.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-loading="loading" max-w="1300px" mx-auto p-5>
+  <div v-loading="loading" max-w="1300px" mx-auto p-5 pb-0>
     <div class="flex">
       <div w-50 aspect-square>
         <CoverLazy :src="songListInfo.picUrl" />
@@ -16,21 +16,30 @@
             创建者ID：{{ songListInfo.userId }}
           </div>
           <div mx="10px" my-5>
-            创建时间：{{ songListInfo.createTime }}
+            创建时间：{{ songListInfo.publishTime }}
           </div>
           <div mx="10px" my-5>
             共{{ songListInfo.trackCount }}首
           </div>
         </div>
-        <!-- <div inline-block mr-2 rounded-2xl bg-orange-700 py-1 px-4 text-white cursor-pointer @click="playSongList(0,tableData)">
+        <div inline-block mr-2 rounded-2xl bg-orange-700 py-1 px-4 text-white cursor-pointer @click="playSongList(0,tableData)">
           播放全部
-        </div> -->
+        </div>
       </div>
     </div>
     <el-tabs v-model="activeName" @tab-click="handleClick">
       <el-tab-pane label="歌曲列表" name="music">
         <div w-full>
-          <MusicTable :data="tableData" :loading="false" :pic="false" :album="false" />
+          <MusicTable
+            :data="tableData"
+            :loading="false"
+            :pic="true"
+            :album="true"
+            :more="more"
+            :max-height="'530px'"
+            style="height: auto;"
+            @load-more="loadMore"
+          />
         </div>
       </el-tab-pane>
       <el-tab-pane label="评论" name="comment" :lazy="true" />
@@ -43,7 +52,6 @@ import { onMounted, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { getSongListDetail } from '@/api/getSongListInfo'
 import { getSongDetail } from '@/api/getSongDetail'
-import useAlbum from '@/hooks/useAlbum'
 
 const loading = ref(true)
 const songListId = ref()
@@ -55,27 +63,54 @@ const params = {
 }
 
 // 通过API获得歌单及歌曲数据
-const songListInfo = ref<SongList>()
-const tableData = ref<Array<Music>>()
+const songListInfo = ref<SongList>({} as SongList)
+const tableData = ref<Music[]>([] as Music[])
+const trackIds = ref<Number[]>([] as Number[])
+const offset = ref(0)
+const more = ref(true)
 
-function init() {
-  getSongListDetail(params).then((res) => {
+async function init() {
+  await getSongListDetail(params).then((res) => {
     songListInfo.value = res
-    return res.trackIds
+    trackIds.value = res.trackIds
+    offset.value = 0
+  })
+  await getSongDetail({
+    ids: getTrackIdsByOffset(offset.value),
+    realIP: params.realIP,
   }).then((res) => {
-    getSongDetail({
-      ids: res.join(','),
-      realIP: params.realIP,
-    }).then((res) => {
-      tableData.value = res
-    })
+    offset.value += 20
+    if (res.length < 20)
+      more.value = false
+    tableData.value = res
   }).then(() => {
     loading.value = false
   })
 }
 
+function getTrackIdsByOffset(currentOffset: number) {
+  return trackIds.value.slice(currentOffset, currentOffset + 20).join(',')
+}
+
 const activeName = ref('music')
 function handleClick(tab: any) {
+}
+
+function playSongList() {
+
+}
+
+function loadMore() {
+  console.log('loadMore')
+  if (!more.value || trackIds.value.length === 0) return
+  getSongDetail({
+    ids: getTrackIdsByOffset(offset.value),
+    realIP: params.realIP,
+  }).then((res) => {
+    offset.value += 20
+    more.value = offset.value < trackIds.value.length
+    tableData.value = tableData.value.concat(res)
+  })
 }
 
 // 播放音乐, 加入队列Hook
@@ -112,3 +147,7 @@ onMounted(() => {
   init()
 })
 </script>
+
+<style lang="scss" scoped>
+
+</style>
