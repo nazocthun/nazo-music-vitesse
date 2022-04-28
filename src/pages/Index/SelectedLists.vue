4dcec598752e70a03e38caccefdc1e2e11a64ca7
nazocthun <nazocthun@gmail.com>
2022-04-28 21:36:42 +0800

    feat: getTags fix: remove scrollbar

--- src/pages/Index/SelectedLists.vue
+++ src/pages/Index/SelectedLists.vue
@@ -1,32 +1,33 @@
 <template>
-  <div max-w="1300px" my-0 mx-auto p-5>
-    <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="全部" name="全部" />
-      <el-tab-pane label="欧美" name="欧美" />
-      <el-tab-pane label="华语" name="华语" />
-    </el-tabs>
-    <CoverView
-      :data="songListsData"
-      :more="more"
-      :show-more="true"
-      @load-more="loadMore"
-      @play="playSongList"
-      @to="toSongList"
-    />
-  </div>
+  <el-scrollbar ref="scrollBar" style="height:100%" @scroll="scroll">
+    <div max-w="1300px" my-0 mx-auto p-5>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="全部" name="全部" />
+        <el-tab-pane v-for="(tag, index) in tags" :key="index" :label="tag.toString()" :name="tag.toString()" />
+      </el-tabs>
+      <CoverView
+        :data="songListsData"
+        :more="more"
+        :show-more="true"
+        @load-more="loadMore"
+        @play="playSongListById"
+        @to="toSongList"
+      />
+    </div>
+  </el-scrollbar>
 </template>
 <script setup lang="ts">
-import { getSelectedSongList } from '@/api/getSongListInfo'
+import { getSelectedSongList, getSelectedTags } from '@/api/getSongListInfo'
+import useSongList from '@/hooks/useSongList'
 
 const router = useRouter()
 const loading = ref(true)
 const activeName = ref('全部')
 const more = ref(true)
+const tags = ref([] as String[])
 const songListsData = ref<SongList[]>([] as SongList[])
 
-function playSongList(id: number) {
-  console.log(`playSongList${id}`)
-}
+const { playSongListById } = useSongList()
 
 function toSongList(id: number) {
   router.push(`/list?id=${id}`)
@@ -39,6 +40,16 @@ const params = reactive({
   realIP: '116.25.146.177',
 })
 
+// 获取标签
+function initTags() {
+  getSelectedTags().then((res) => {
+    tags.value = res
+  })
+}
+
+onMounted(() => initTags())
+
+// 通过标签获取歌单列表
 function init() {
   loading.value = true
   params.cat = activeName.value
@@ -55,6 +66,8 @@ function init() {
 function handleClick() {
   init()
 }
+
+// 滚动加载
 async function loadMore() {
   await getSelectedSongList(params).then((res) => {
     if (res.songList.length === 0) return
@@ -64,4 +77,21 @@ async function loadMore() {
     more.value = res.more
   })
 }
+
+// keep-alive记住滚动条位置
+const currentScrollTop = ref(0)
+function scroll({ scrollTop }: { scrollTop: number }) {
+  currentScrollTop.value = scrollTop
+}
+const scrollBar = ref()
+onActivated(() => scrollBar.value.setScrollTop(currentScrollTop.value))
 </script>
+
+<route>
+{
+  name: "selectedlists",
+  meta: {
+    keepAlive: true,
+  }
+}
+</route>
