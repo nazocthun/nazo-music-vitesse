8c2f3cf13fd451b390e457f19a43bd9a2fd5fd94
nazocthun <nazocthun@gmail.com>
2022-04-28 21:40:45 +0800

    feat: SongList hook

---
+++ src/hooks/useSongList.ts
@@ -0,0 +1,48 @@
+import usePlay from '@/hooks/usePlay'
+import { getSongDetail } from '@/api/getSongDetail'
+import { useMusicQueueStore } from '@/store/MusicQueueStore'
+import { getSongListDetail } from '@/api/getSongListInfo'
+
+export default function useSongList() {
+  const MUSIC_QUEUE_STORE = useMusicQueueStore()
+  const { getMusicInfo } = usePlay()
+  const { changeNowIndexTo, addToQueueWith } = MUSIC_QUEUE_STORE
+  async function playSongList(trackIds: Number[]) {
+    let allMusic: Music[] = []
+    if (trackIds.length !== 0) {
+      await getSongDetail({
+        ids: trackIds.join(','),
+        realIP: '116.25.146.177',
+      }).then((res) => {
+        allMusic = res ?? []
+      })
+    }
+    if (allMusic.length > 0) {
+      MUSIC_QUEUE_STORE.resetQueue()
+      for (const music of allMusic)
+        addToQueueWith(music)
+      changeNowIndexTo(0)
+      getMusicInfo(allMusic[0], 'queue')
+      return
+    }
+    ElMessage({
+      type: 'error',
+      message: '网络好像有点问题，刷新下试试？',
+      showClose: true,
+    })
+  }
+  async function playSongListById(id: Number) {
+    await getSongListDetail({
+      id,
+      realIP: '116.25.146.177',
+    }).then((res) => {
+      return res.trackIds
+    }).then((res) => {
+      playSongList(res)
+    })
+  }
+  return {
+    playSongList,
+    playSongListById,
+  }
+}
