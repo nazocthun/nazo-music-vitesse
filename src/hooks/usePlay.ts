24df6bd81067c39b97f482bc5f11be34cd83991a
nazocthun <nazocthun@gmail.com>
2022-04-28 21:41:33 +0800

    fix: pause music reset bug

--- src/hooks/usePlay.ts
+++ src/hooks/usePlay.ts
@@ -8,11 +8,7 @@ export default function usePlay() {
   const MUSIC_INFO_STORE = useMusicInfoStore()
   const PLAY_STORE = usePlayStore()
   // 播放音乐
-  const { musicChanged } = usePlayStore()
-
   function getMusicInfo(row: Music, from: 'doubleclick' | 'plus' | 'queue') {
-    // console.log(row)
-    // console.log(playType)
     PLAY_STORE.$patch((state) => {
       state.location = from
     })
@@ -22,14 +18,13 @@ export default function usePlay() {
     }
     const musicInfo: Music = row
     getMusicUrl(params).then((res) => {
-      const musicUrl = res.musicUrl
+      const musicUrl = res[0]
       MUSIC_INFO_STORE.$patch((state) => {
         state.currentMusicUrl = musicUrl
         state.currentMusicInfo = musicInfo
       })
     })
     addToQueue(row, from)
-    musicChanged()
   }
 
   function rowDoubleClick(row: Music) {
