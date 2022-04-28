b822e6ea0b218f1039bb2b76243e8d865d944acf
nazocthun <nazocthun@gmail.com>
2022-04-28 21:37:43 +0800

    feat: getSimilarSongList

--- src/api/getSongListInfo.ts
+++ src/api/getSongListInfo.ts
@@ -7,6 +7,44 @@ const REQUEST_URL = {
   selected: '/top/playlist/highquality',
   tracks: '/playlist/track/all',
   detail: '/playlist/detail',
+  selectedtags: 'playlist/highquality/tags',
+  related: '/related/playlist',
+}
+// 获取相似歌单
+export const convertSimilarSongList = async(res: AxiosResponse<any>): Promise<SongList[]> => {
+  const songLists = res.data.playlists
+  return songLists.map((item: { id: any; name: any; coverImgUrl: string; creator: { userId: any } }) => {
+    return {
+      id: item.id,
+      name: item.name,
+      picUrl: getCompressedImgUrl(item.coverImgUrl, 500),
+      userId: item.creator.userId,
+    }
+  })
+}
+
+/**
+ *
+ * @param params.id 歌单id
+ * @returns SongList[] 相似歌单列表
+ */
+export const getSimilarSongList = async(params = {}) => {
+  return getRequest(REQUEST_URL.related, params).then(convertSimilarSongList)
+}
+
+// 获取精选歌单标签
+export const convertSelectedTags = async(res: AxiosResponse<any>): Promise<String[]> => {
+  const tags = res.data.tags
+  return tags.map((item: { name: string }) => {
+    return item.name
+  })
+}
+/**
+ *
+ * @returns string[], 歌单标签
+ */
+export const getSelectedTags = (params = {}) => {
+  return getRequest(REQUEST_URL.selectedtags, params).then(convertSelectedTags)
 }
 
 // 获取精选歌单
@@ -29,6 +67,13 @@ export const convertSelectedSongList = async(res: AxiosResponse<any>): Promise<S
   }
 }
 
+/**
+ *
+ * @param params.cat 标签
+ * @param params.limit 数量
+ * @param params.before 分页参数, 取上一页最后一个歌单的 updateTime 获取下一页数据
+ * @returns SongListWrapper 精品歌单列表
+ */
 export const getSelectedSongList = (params = {}) => {
   return getRequest(REQUEST_URL.selected, params).then(convertSelectedSongList)
 }
@@ -51,6 +96,11 @@ export const convertSongListDetail = async(res: AxiosResponse<any>): Promise<Son
   }
 }
 
+/**
+ *
+ * @param params.id string 歌单id
+ * @returns SongList 歌单详情
+ */
 export const getSongListDetail = (params = {}) => {
   return getRequest(REQUEST_URL.detail, params).then(convertSongListDetail)
 }
