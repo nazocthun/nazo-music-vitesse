385e6111af0517188a32e690ebba5ce7a591b1a0
nazocthun <nazocthun@gmail.com>
2022-04-28 21:35:00 +0800

    feat: new mv page

---
+++ src/pages/MV/mv.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ mvId }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const mvId = ref()
+
+onMounted(() => {
+  mvId.value = route.query.id
+})
+</script>
+
+<route>
+{
+  name: "mv",
+  meta: {
+    keepAlive: true,
+  }
+}
+</route>
