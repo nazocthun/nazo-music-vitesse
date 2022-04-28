e37d12d479498541a707a80455356e3885f9f6bb
nazocthun <nazocthun@gmail.com>
2022-04-28 21:40:06 +0800

    style: change to default

--- src/components/NavBar/SearchBox.vue
+++ src/components/NavBar/SearchBox.vue
@@ -18,27 +18,27 @@ function search() {
 </script>
 
 <style lang="scss">
-$search-icon-color: rgba(0, 0, 0, 0.5);
+// $search-icon-color: rgba(0, 0, 0, 0.5);
 
-.searchbox input {
-  width: 200px;
-  border-radius: 20px;
-  box-shadow: none;
-  outline: none;
-  font-size: 14px;
-  background-color: rgba(63, 114, 175, 0.7);
-  --el-input-hover-border-color: transparent;
-  --el-input-focus-border-color: transparent;
-  --el-input-text-color: rgba(0, 0, 0, 0.7);
-  --el-input-placeholder-color: rgba(0, 0, 0, 0.3);
-}
+// .searchbox input {
+//   width: 200px;
+//   border-radius: 20px;
+//   box-shadow: none;
+//   outline: none;
+//   font-size: 14px;
+//   background-color: rgba(63, 114, 175, 0.7);
+//   --el-input-hover-border-color: transparent;
+//   --el-input-focus-border-color: transparent;
+//   --el-input-text-color: rgba(0, 0, 0, 0.7);
+//   --el-input-placeholder-color: rgba(0, 0, 0, 0.3);
+// }
 
-.searchbar{
-  margin: auto 8px auto auto ;
-  box-shadow: 12px 12px 2px 1px rgba(0, 0, 0, 1);
-}
+// .searchbar{
+//   margin: auto 8px auto auto ;
+//   box-shadow: 12px 12px 2px 1px rgba(0, 0, 0, 1);
+// }
 
-.el-input__suffix {
-  color: $search-icon-color;
-}
+// .el-input__suffix {
+//   color: $search-icon-color;
+// }
 </style>
