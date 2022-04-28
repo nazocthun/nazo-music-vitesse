54aea83d9a54f69ec33220a6be2cae907c3e55aa
nazocthun <nazocthun@gmail.com>
2022-04-28 21:38:19 +0800

    docs: API docs

--- src/api/getMusicUrl.ts
+++ src/api/getMusicUrl.ts
@@ -5,12 +5,17 @@ const REQUEST_URL = {
   songUrl: '/song/url',
 }
 
-export const convertMusicUrl = async(res: AxiosResponse<any>): Promise<musicUrl> => {
-  return {
-    musicUrl: res.data.data[0].url,
-  }
+export const convertMusicUrl = async(res: AxiosResponse<any>): Promise<string[]> => {
+  return res.data.data.map((item: { url: any }) => item.url)
 }
 
+/**
+ * 获取音乐url
+ * @param {string} params.id - 音乐 id 多个可用,分隔
+ * @param {number} params.br  - 码率，默认999000
+ * @returns {string[]}  音乐 url 列表
+ *
+ */
 export const getMusicUrl = (params = {}) => {
   return getRequest(REQUEST_URL.songUrl, params).then(convertMusicUrl)
 }
