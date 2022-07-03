e80b5053a185db81e9a70d129e8923bacc8c602f
nazocthun <nazocthun@gmail.com>
2022-07-03 10:27:04 +0800

    feat: index page

--- src/pages/Index/index.vue
+++ src/pages/Index/index.vue
@@ -1,13 +1,62 @@
 <template>
-  <div>
-    Discover Page
-  </div>
+  <el-scrollbar ref="scrollBar" style="height:100%" @scroll="scroll">
+    <div class="whole-page" mx-auto p-5>
+      <BannerView m-3 :banners="banners" />
+      <div m-3 text="xl" font-bold>推荐歌单</div>
+      <CoverView
+        :data="personlizedSongList"
+        :more="false"
+        :show-more="false"
+        @play="playSongListById"
+        @to="toSongList"
+      />
+    </div>
+  </el-scrollbar>
 </template>
 
 <script setup lang="ts">
+import { getBanner } from '@/api/getBanner'
+import { getPersonalizedSongList } from '@/api/getPersonalized'
+import useSongList from '@/hooks/useSongList'
 
+const router = useRouter()
+const banners = ref<Banner[]>([])
+const personlizedSongList = ref<SongList[]>([])
+const { playSongListById } = useSongList()
+
+// keep-alive记住滚动条位置
+const currentScrollTop = ref(0)
+function scroll({ scrollTop }: { scrollTop: number }) {
+  currentScrollTop.value = scrollTop
+}
+const scrollBar = ref()
+onActivated(() => scrollBar.value.setScrollTop(currentScrollTop.value))
+
+onMounted(() => init())
+function init() {
+  getBanner({ type: 0 }).then((res) => {
+    banners.value = res
+  })
+  getPersonalizedSongList({ limit: 10 }).then((res) => {
+    personlizedSongList.value = res
+    console.log(personlizedSongList.value)
+  })
+}
+
+function toSongList(id: number) {
+  router.push(`/list/${id}`)
+}
 </script>
 
-<style scoped>
+<style>
 
 </style>
+
+<route>
+{
+  name: "discover",
+  meta: {
+    keepAlive: true,
+  }
+}
+</route>
