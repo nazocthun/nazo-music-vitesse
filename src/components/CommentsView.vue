7c3debf119c8ef6365782369db7793ab5cfbb9c0
nazocthun <nazocthun@gmail.com>
2022-07-03 10:26:09 +0800

    feat: comments view

---
+++ src/components/CommentsView.vue
@@ -0,0 +1,17 @@
+<template>
+  {{ data }}
+</template>
+
+<script setup lang="ts">
+import { getAlbumComment } from '@/api/getAlbumInfo'
+
+const data = ref()
+await getAlbumComment({ id: 147476181 }).then((res) => {
+  console.log(res)
+  data.value = res
+})
+</script>
+
+<style>
+
+</style>
