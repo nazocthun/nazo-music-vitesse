f6c1f9c042f5c2688c10fbe00b24ce68907945af
nazocthun <nazocthun@gmail.com>
2022-07-03 10:25:51 +0800

    feat: get username by userId

---
+++ src/api/getUserName.ts
@@ -0,0 +1,19 @@
+import type { AxiosResponse } from 'axios'
+import { getRequest } from './request'
+
+const REQUEST_URL = {
+  detail: '/user/detail',
+}
+
+export const convertUserDetail = async(res: AxiosResponse<any>): Promise<User> => {
+  const profile = res.data.profile
+  return {
+    userId: profile.userId,
+    nickname: profile.nickname,
+    avatarUrl: profile.avatarUrl,
+  }
+}
+
+export const getUserDetail = (params = {}) => {
+  return getRequest(REQUEST_URL.detail, params).then(convertUserDetail)
+}
