4eebd8b0b33f3a7a68043414ed0405643a76f868
nazocthun <nazocthun@gmail.com>
2022-07-03 10:25:20 +0800

    feat: banner

---
+++ src/api/getBanner.ts
@@ -0,0 +1,21 @@
+import type { AxiosResponse } from 'axios'
+import { getRequest } from './request'
+
+const REQUEST_URL = {
+  detail: '/banner',
+}
+
+export const convertBanner = async(res: AxiosResponse<any>): Promise<Banner[]> => {
+  const banners = res.data.banners
+  return banners.map((item: { imageUrl: any; targetType: any; targetId: any }) => {
+    return {
+      imageUrl: item.imageUrl,
+      targetType: item.targetType,
+      targetId: item.targetId,
+    }
+  })
+}
+
+export const getBanner = (params = { type: 0 }) => {
+  return getRequest(REQUEST_URL.detail, params).then(convertBanner)
+}
