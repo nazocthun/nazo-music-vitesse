542978cd8f6b7176bb077f67f59ca34fd203d6a3
nazocthun <nazocthun@gmail.com>
2022-07-21 13:26:59 +0800

    feat: comment component

---
+++ src/components/CommentBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div flex m-3 mb-6 cursor-default>
+    <img w-12 h-12 rounded-full border="1.5px solid orange-700" mr-2 :src="props.comment.user.avatarUrl" alt="">
+    <div flex-col w-full>
+      <div mb-2 text-sm>
+        <span text-sky-600>
+          {{ props.comment.user.nickname }}
+        </span>
+        <span>
+          ：{{ props.comment.content }}
+        </span>
+      </div>
+      <div v-if="props.comment.beRepliedComment.length !== 0" bg-gray-100 my-2 p-2>
+        <div v-for="(replies, reply_index) in comment.beRepliedComment" :key="reply_index" flex mb-2 text-sm>
+          <div text-sky-600>
+            {{ replies.user.nickname }}
+          </div>
+          <div>
+            ：{{ replies.content }}
+          </div>
+        </div>
+      </div>
+      <div flex relative bottom-0 items-center text-gray-500 text-sm border-b-1 border-gray-200 pb-6>
+        <div w-24>
+          {{ props.comment.timeStr }}
+        </div>
+        <div ml-12 i-carbon-thumbs-up />
+        <div ml-1>
+          {{ props.comment.likedCount }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  comment: {
+    default() {
+      return {} as MusicComment
+    },
+    required: true,
+  },
+})
+
+</script>
--- src/components/CommentsView.vue
+++ src/components/CommentsView.vue
@@ -1,14 +1,60 @@
 <template>
-  {{ data }}
+  <!-- {{ data }} -->
+  <div v-if="comments.length !== 0">
+    <div mt-3 mb-6 font-semibold text-2xl>
+      热门评论
+    </div>
+    <div v-for="(hotComment, index1) in hotComments" :key="index1">
+      <CommentBar :comment="hotComment" />
+    </div>
+    <div mt-3 mb-6 font-semibold text-2xl>
+      最新评论
+    </div>
+    <div v-for="(comment,index2) in comments" :key="index2">
+      <CommentBar :comment="comment" />
+    </div>
+  </div>
+  <div v-else mt-3 mb-6 font-semibold text-2xl>
+    暂无评论
+  </div>
 </template>
 
 <script setup lang="ts">
-import { getAlbumComment } from '@/api/getAlbumInfo'
+import { getComment, getHotComment } from '@/api/getComments'
 
-const data = ref()
-await getAlbumComment({ id: 147476181 }).then((res) => {
-  console.log(res)
-  data.value = res
+const props = defineProps({
+  id: {
+    default() {
+      return ''
+    },
+    required: true,
+  },
+  type: {
+    default() {
+      return 3
+    },
+    required: true,
+  },
+})
+
+const hotComments = ref<MusicComment[]>([] as MusicComment[])
+const comments = ref<MusicComment[]>([] as MusicComment[])
+
+function init() {
+  getHotComment({ id: props.id, type: props.type, limit: 10 }).then((res) => {
+    hotComments.value = res
+  })
+  getComment({ id: props.id, type: props.type, sortType: 3 }).then((res) => {
+    console.log(res)
+    comments.value = res
+  })
+
+}
+
+onMounted(() => init())
+
+watch(() => props.id, () => {
+  init()
 })
 </script>
 
