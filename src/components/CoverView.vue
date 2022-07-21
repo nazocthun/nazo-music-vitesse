9748d5a57ce6990083cb459399b7852086e57565
nazocthun <nazocthun@gmail.com>
2022-07-21 13:29:06 +0800

    feat: new type check

--- src/components/CoverView.vue
+++ src/components/CoverView.vue
@@ -2,47 +2,37 @@
   <div>
     <div>
       <ul
-        v-infinite-scroll="loadMore"
-        infinite-scroll-delay="700"
-        grid grid-cols-5 gap-5 items-center list-none pl-0 m-3
-      >
+        v-infinite-scroll="loadMore" infinite-scroll-delay="700" grid grid-cols-5 gap-5 items-center list-none pl-0
+        m-3>
         <li v-for="(item, index1) in props.data" :key="index1" text-sm w-full>
           <div
-            class="group"
-            :class="{
-              'aspect-video': item.hasOwnProperty('playCount'),
-              'aspect-square': !item.hasOwnProperty('playCount')
-            }"
-            w-full relative cursor-pointer transition-all
-            hover="scale-105 transition-all"
-            @click="$emit('to', item.id)"
-          >
+            class="group" :class="{
+              'aspect-video': isMV(item),
+              'aspect-square': isAlbum(item) || isSonglist(item),
+            }" w-full relative cursor-pointer transition-all hover="scale-105 transition-all"
+            @click="$emit('to', item.id)">
             <CoverLazy :src="item.picUrl" />
             <div
-              absolute justify-center items-center
-              w-12 h-12 top="50%" left="50%" translate-x="-50%" translate-y="-50%"
-              m-0 rounded-full opacity-0 group-hover="bg-white opacity-85 transition-all" transition-all
-              @click.stop="$emit('play', item.id)"
-            >
-              <div
-                w-full h-full bg-orange-700
-                i-ic-sharp-play-circle-outline
-              />
+              absolute justify-center items-center w-12 h-12 top="50%" left="50%" translate-x="-50%"
+              translate-y="-50%" m-0 rounded-full opacity-0 group-hover="bg-white opacity-85 transition-all"
+              hover="scale-105 transition-all" transition-all @click.stop="$emit('play', item.id)">
+              <div w-full h-full bg-orange-700 i-ic-sharp-play-circle-outline />
             </div>
             <div
-              v-if="item.hasOwnProperty('playCount')"
-              flex items-center absolute top-1 right-1
-              text-white text-xs cursor-default drop-shadow="[0_0_2px_rgba(0,0,0,1)]"
-            >
+              v-if="isMV(item)" flex items-center absolute top-1 right-1 text-white text-xs cursor-default
+              drop-shadow="[0_0_2px_rgba(0,0,0,1)]">
               <div i-ic-sharp-play-arrow w-3 h-3 />
-              {{ (item as MV).playCount }}
+              {{ item.playCount }}
             </div>
           </div>
           <div :title="item.name" my="1.5" truncate text-sm cursor-default font-medium text-stone-700>
             {{ item.name }}
           </div>
-          <div text-xs cursor-default text-stone-400>
-            {{ item.hasOwnProperty('updateTime') ? `更新时间：${(item as SongList).updateTime}` : `发布时间：${item.publishTime}` }}
+          <div v-if="!isSimpleSonglist(item) && props.showTime" text-xs cursor-default text-stone-400>
+            {{ isSonglist(item) ? `更新时间：${item.updateTime}` : `发布时间：${item.publishTime}` }}
+          </div>
+          <div v-if="!props.showTime" text-xs cursor-default text-stone-400>
+            {{ `${(item as MV).artist.name}` }}
           </div>
         </li>
       </ul>
@@ -71,11 +61,22 @@ const props = defineProps({
     },
     required: true,
   },
+  showTime: {
+    default() {
+      return true
+    },
+    required: false,
+  },
 })
 
-const em = defineEmits(['load-more', 'play', 'to'])
+const em = defineEmits(['loadMore', 'play', 'to'])
+
+const isAlbum = (props: any): props is Album => (props as Album).size !== undefined
+const isSonglist = (props: any): props is SongList => (props as SongList).trackCount !== undefined || (props as SongList).userId !== undefined
+const isSimpleSonglist = (props: any): props is SongList => (props as SongList).userId !== undefined
+const isMV = (props: any): props is MV => !isAlbum(props) && !isSonglist(props)
 
 function loadMore() {
-  em('load-more')
+  em('loadMore')
 }
 </script>
