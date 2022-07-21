703e8ba8f54211899744c97449cf5a4cc8a043f1
nazocthun <nazocthun@gmail.com>
2022-07-21 13:34:04 +0800

    feat: remember scrollbar

--- src/pages/album/[albumId].vue
+++ src/pages/album/[albumId].vue
@@ -1,50 +1,54 @@
 <template>
-  <div v-loading="loading" class="whole-page" mx-auto p-5>
-    <div class="flex">
-      <div w-50 aspect-square>
-        <CoverLazy :src="albumInfo.picUrl" />
+  <el-scrollbar ref="scrollBar" @scroll="scroll">
+    <div v-loading="loading" class="whole-page" mx-auto p-5>
+      <div class="flex">
+        <div w-50 aspect-square>
+          <CoverLazy :src="albumInfo.picUrl" />
+        </div>
+        <div px-5 py-0 flex-1>
+          <div text-2xl font-bold flex items-center>
+            <div text-orange-700 border="~ solid orange-700" inline-block text-xl mr-2 rounded px-2 py-0 cursor-default>
+              专辑
+            </div>
+            {{ albumInfo.name }}
+          </div>
+          <div items-center text-sm>
+            <div mx="10px" my-5>
+              歌手：{{ artistName }}
+            </div>
+            <div mx="10px" my-5>
+              时间：{{ albumInfo.publishTime }}
+            </div>
+            <div mx="10px" my-5>
+              共{{ albumInfo.size }}首
+            </div>
+          </div>
+          <div
+            inline-block mr-2 rounded-2xl bg-orange-700 py-1 px-4 text-white cursor-pointer
+            @click="playAlbum(0,tableData)"
+          >
+            播放全部
+          </div>
+        </div>
       </div>
-      <div px-5 py-0 flex-1>
-        <div text-2xl font-bold flex items-center>
-          <div text-orange-700 border="~ solid orange-700" inline-block text-xl mr-2 rounded px-2 py-0 cursor-default>
-            专辑
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="歌曲列表" name="music">
+          <div w-full>
+            <MusicTable :data="tableData" :loading="false" :pic="false" :album="false" :show-more="false" />
           </div>
-          {{ albumInfo.name }}
-        </div>
-        <div items-center text-sm>
-          <div mx="10px" my-5>
-            歌手：{{ artistName }}
-          </div>
-          <div mx="10px" my-5>
-            时间：{{ albumInfo.publishTime }}
-          </div>
-          <div mx="10px" my-5>
-            共{{ albumInfo.size }}首
-          </div>
-        </div>
-        <div
-          inline-block mr-2 rounded-2xl bg-orange-700 py-1 px-4 text-white cursor-pointer
-          @click="playAlbum(0,tableData)"
-        >
-          播放全部
-        </div>
-      </div>
+        </el-tab-pane>
+        <el-tab-pane label="专辑简介" name="descrption" :lazy="true">
+          <span font-bold text-xl inline-block mx-0 my="2.5">专辑介绍</span>
+          <p v-for="(item, index) in albumInfo.description" :key="index" indent-8 my-4 mx-0 text-base>
+            {{ item }}
+          </p>
+        </el-tab-pane>
+        <el-tab-pane label="评论" name="comment" :lazy="true">
+          <CommentsView :id="props.albumId" :type="3" />
+        </el-tab-pane>
+      </el-tabs>
     </div>
-    <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="歌曲列表" name="music">
-        <div w-full>
-          <MusicTable :data="tableData" :loading="false" :pic="false" :album="false" :show-more="false" />
-        </div>
-      </el-tab-pane>
-      <el-tab-pane label="专辑简介" name="descrption" :lazy="true">
-        <span font-bold text-xl inline-block mx-0 my="2.5">专辑介绍</span>
-        <p v-for="(item, index) in albumInfo.description" :key="index" indent-8 my-4 mx-0 text-base>
-          {{ item }}
-        </p>
-      </el-tab-pane>
-      <el-tab-pane label="评论" name="comment" :lazy="true" />
-    </el-tabs>
-  </div>
+  </el-scrollbar>
 </template>
 
 <script setup lang="ts">
@@ -83,6 +87,14 @@ function handleClick(tab: any) {
 
 // 播放音乐, 加入队列Hook
 const { playAlbum } = useAlbum()
+
+// keep-alive记住滚动条位置
+const currentScrollTop = ref(0)
+function scroll({ scrollTop }: { scrollTop: number }) {
+  currentScrollTop.value = scrollTop
+}
+const scrollBar = ref()
+onActivated(() => scrollBar.value.setScrollTop(currentScrollTop.value))
 
 // 监视路由变化
 watch(() => props.albumId, () => {
